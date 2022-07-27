20ecd2adc0bc43e7d6ecdeb259473f02b1eecb20
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-07-27 22:49:49 +0530

    Time: 25 ms (10.62%), Space: 70.3 MB (6.06%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,6 +1,7 @@
 class Solution {
     
-    private void getMaxMin(int[] nums, int[] maxMin) {
+    //Function will build maxMin array with minimum and maximum element of nums
+    private void getMaxMin(int[] nums, int[] maxMin) { 
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         
@@ -12,6 +13,7 @@ class Solution {
         maxMin[1] = max;
     }
     
+    //Function will return the count of element larger than mid in nums[]
     private int getLargerCount(int[] nums, int mid) {
         int count = 0;
         for(int e : nums) {
@@ -21,15 +23,22 @@ class Solution {
     }
     
     public int findKthLargest(int[] nums, int k) {
+        /* Approach 
+        - TC --> O(N * log(Range)) || SC --> O(1)
+        - Kth largest element will be present only between minimu and maximum element of nums[]\
+        - So we apply binary search on that range
+        - for searching, if count of element greater than (k-1) :: that means, we need a more larger number, so discarding left half
+        - else we zero down to the (k-1)th element :: basically store the answer and move left i.e discard right half
+        */
         int[] maxMin = new int[2];
-        getMaxMin(nums, maxMin);
+        getMaxMin(nums, maxMin); //TC --> O(N)
         
-        int low = maxMin[0], high = maxMin[1];
+        int low = maxMin[0], high = maxMin[1]; //Range
         int ans = -1;
-        while(low <= high) {
+        while(low <= high) { //TC --> O(log(Range))
             int mid = (low + high) / 2;
             
-            int count = getLargerCount(nums, mid);
+            int count = getLargerCount(nums, mid); //TC --> O(N)
             
             if(count > k-1) {
                 low = mid + 1;

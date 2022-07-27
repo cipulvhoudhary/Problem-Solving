d8d9751b36df2b3f2ea10d7d253ca821df110dbe
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-07-27 17:42:42 +0530

    Time: 1 ms (74.47%), Space: 43 MB (18.63%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,58 @@
+class Solution {
+    private int getPivot(int[] nums) { //TC --> O(logN) || SC --> O(1)
+        int N = nums.length;
+        int low = 0, high = N-1;
+        int pivot = 0;
+        
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] >= nums[0]) {
+                low = mid + 1;
+            }
+            else {
+                pivot = mid;
+                high = mid - 1;
+            }
+        }
+        return pivot;
+    }
+    
+    private int searchUtil(int[] nums, int target, int low, int high) { //TC --> O(logN) || SC --> O(1)
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] < target) {
+                low = mid + 1;
+            }
+            else if(nums[mid] > target) {
+                high = mid - 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return-1;
+    }
+    
+    public int search(int[] nums, int target) {
+        /* Approach 
+        - Find the pivot point (point from which the elements are rotated)
+        - two half will be there --> [0, pivot-1] && [pivot, N-1]
+        - according possible occurence in the two half, apply binary search for target
+        //TC --> O(logN) || SC --> O(1)
+        */
+        
+        if(nums[0] == target) {
+            return 0;
+        }
+        
+        int N = nums.length;
+        int pivot = getPivot(nums);
+        
+        if(target > nums[0] && pivot != 0) {
+            return searchUtil(nums, target, 0, pivot-1);
+        }
+        else {
+            return searchUtil(nums, target, pivot, N-1);
+        }
+    }
+}

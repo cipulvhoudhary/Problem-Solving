b2e7ff65de4077830e43555c6da39e3b38d7c406
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-09-02 10:15:21 +0530

    Time: 846 ms (17.44%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -2,7 +2,8 @@
 SELECT 
     e.name AS Employee
 FROM    
-    Employee e JOIN Employee m
-    ON e.managerId = m.id 
+    Employee e 
+        JOIN 
+    Employee m ON e.managerId = m.id 
 WHERE 
     e.salary > m.salary;

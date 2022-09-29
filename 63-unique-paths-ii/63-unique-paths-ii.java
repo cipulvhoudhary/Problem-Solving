14d037a4ea517f3180f9feb3d5901d4eb4bff7f6
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-09-29 11:48:24 +0530

    Time: 1 ms (70.75%), Space: 42.8 MB (15.82%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     
-    private int uniquePathsWithObstaclesUtil(int x, int y, int[][] obstacleGrid, int[][] dp) {
+    
+    private int uniquePathsWithObstaclesMemoizationUtil(int x, int y, int[][] obstacleGrid, int[][] dp) {
         // Boundary - case
         if(x < 0 || y < 0) return 0;
         // Obstacle check
@@ -11,23 +12,68 @@ class Solution {
         
         if(dp[x][y] == -1) {
             // Main logic : we can either move up or left
-            int top = uniquePathsWithObstaclesUtil(x-1, y, obstacleGrid, dp);
-            int left = uniquePathsWithObstaclesUtil(x, y-1, obstacleGrid, dp);
+            int top = uniquePathsWithObstaclesMemoizationUtil(x-1, y, obstacleGrid, dp);
+            int left = uniquePathsWithObstaclesMemoizationUtil(x, y-1, obstacleGrid, dp);
             dp[x][y] = top + left;
         }
         
         return dp[x][y];
     }
     
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    private int uniquePathsWithObstaclesTabulationUtil(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         
         int[][] dp = new int[m][n];
-        for(int[] row : dp) {
-            Arrays.fill(row, -1);
+        
+        // Base - case 
+        boolean rowObstacle = false;
+        for(int row=0; row<m; row++) {
+            if(obstacleGrid[row][0] == 1) {
+                rowObstacle = true;
+            }
+            if(!rowObstacle) {
+                dp[row][0] = 1;
+            }
         }
         
-        return uniquePathsWithObstaclesUtil(m-1, n-1, obstacleGrid, dp);
+        boolean colObstacle = false;
+        for(int col=0; col<n; col++) {
+            if(obstacleGrid[0][col] == 1) {
+                colObstacle = true;
+            }
+            if(!colObstacle) {
+                dp[0][col] = 1;
+            }
+        }
+        
+        // Main - logic
+        for(int row=1; row<m; row++) {
+            for(int col=1; col<n; col++) {
+                if(obstacleGrid[row][col] == 1) {
+                    dp[row][col] = 0;
+                }
+                else {
+                    dp[row][col] = dp[row-1][col] + dp[row][col-1]; // left + top
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+//         int m = obstacleGrid.length;
+//         int n = obstacleGrid[0].length;
+        
+//         int[][] dp = new int[m][n];
+//         for(int[] row : dp) {
+//             Arrays.fill(row, -1);
+//         }
+        
+//         return uniquePathsWithObstaclesMemoizationUtil(m-1, n-1, obstacleGrid, dp);
+        
+        return uniquePathsWithObstaclesTabulationUtil(obstacleGrid);
+        
     }
 }

2a715a4bba958873ff8a92f7c305108d189f31b6
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-09-30 19:13:16 +0530

    Time: 28 ms (10.51%), Space: 47.3 MB (71.25%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -19,6 +19,7 @@ class Solution {
         return dp[x][y];
     }
     
+    // Tabulation :: TC --> O(N*N) || SC --> O(N*N)
     private int minFallingPathSumTabulationUtil(int [][] matrix) {
         int n = matrix.length;
         
@@ -50,6 +51,41 @@ class Solution {
         }
         return minSum;
     }
+    
+    // Space optimized :: TC --> O(N*N) || SC --> O(N)
+    private int minFallingPathSumSpaceOptimizedUtil(int [][] matrix) {
+        int n = matrix.length;
+        
+        int[] dp = new int[n];
+        
+        if(n == 1) return matrix[0][0]; // Edge - case
+    
+        for(int col=0; col<n; col++) {
+            dp[col] = matrix[0][col];
+        }
+        
+        int minSum = Integer.MAX_VALUE;
+        
+        for(int row=1; row < n; row++) {
+            int[] temp = new int[n];
+            for(int col=0; col<n; col++) {
+                int top = dp[col];
+                int topLeft = Integer.MAX_VALUE;
+                int topRight = Integer.MAX_VALUE;
+                if(col-1 >= 0) topLeft = dp[col-1];
+                if(col+1 < n) topRight = dp[col+1];
+                int sum = matrix[row][col] + Math.min(top, Math.min(topLeft, topRight));
+                temp[col] = sum;
+                
+                if(row == n-1) {
+                    minSum = Math.min(minSum, sum);
+                }
+            }
+            dp = temp;
+        }
+        return minSum;
+    }
+    
     public int minFallingPathSum(int[][] matrix) {
 //         int n = matrix.length;
         
@@ -67,7 +103,10 @@ class Solution {
 //         return minSum;
         
         // Tabulation
-        return minFallingPathSumTabulationUtil(matrix);
+        // return minFallingPathSumTabulationUtil(matrix);
+        
+        // Space Optimized
+        return minFallingPathSumSpaceOptimizedUtil(matrix);
     }
     
 }

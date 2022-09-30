fc1f4119ce5457edb0dff95f697fafc147304117
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-09-30 16:48:55 +0530

    Time: 41 ms (6.24%), Space: 48.2 MB (13.51%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     
+    // Memoization :: TC --> O(N*N) || SC --> O(N*N)
     private int minFallingPathSumUtil(int x, int y, int[][] matrix, int[][] dp) {
         // Boundary case
         if(x < 0 || y < 0 || y >= matrix.length) return Integer.MAX_VALUE;
@@ -16,24 +17,57 @@ class Solution {
             dp[x][y] = matrix[x][y] + Math.min(top, Math.min(topLeft, topRight));
         }
         return dp[x][y];
-        
     }
     
-    public int minFallingPathSum(int[][] matrix) {
+    private int minFallingPathSumTabulationUtil(int [][] matrix) {
         int n = matrix.length;
         
+        int[][] dp = new int[n][n];
+        
+        if(n == 1) return matrix[0][0]; // Edge - case
+    
+        for(int col=0; col<n; col++) {
+            dp[0][col] = matrix[0][col];
+        }
         
         int minSum = Integer.MAX_VALUE;
-        for(int col=0; col<n; col++) {
-            
-            int[][] dp = new int[n][n];
-            for(int[] row : dp) {
-                Arrays.fill(row, -1);
+        
+        for(int row=1; row < n; row++) {
+            for(int col=0; col<n; col++) {
+                int top = dp[row-1][col];
+                int topLeft = Integer.MAX_VALUE;
+                int topRight = Integer.MAX_VALUE;
+                if(col-1 >= 0) topLeft = dp[row-1][col-1];
+                if(col+1 < n) topRight = dp[row-1][col+1];
+                
+                int sum = matrix[row][col] + Math.min(top, Math.min(topLeft, topRight));
+                dp[row][col] = sum;
+                
+                if(row == n-1) {
+                    minSum = Math.min(minSum, sum);
+                }
             }
-            
-            minSum = Math.min(minSum, minFallingPathSumUtil(n-1, col, matrix, dp));
         }
         return minSum;
+    }
+    public int minFallingPathSum(int[][] matrix) {
+//         int n = matrix.length;
+        
+        
+//         int minSum = Integer.MAX_VALUE;
+//         for(int col=0; col<n; col++) {
+            
+//             int[][] dp = new int[n][n];
+//             for(int[] row : dp) {
+//                 Arrays.fill(row, -1);
+//             }
+            
+//             minSum = Math.min(minSum, minFallingPathSumUtil(n-1, col, matrix, dp));
+//         }
+//         return minSum;
+        
+        // Tabulation
+        return minFallingPathSumTabulationUtil(matrix);
     }
     
 }

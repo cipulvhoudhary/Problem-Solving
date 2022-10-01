ff12f033c6bf3e066efeb6d9a2478db20a4bef56
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-10-01 17:25:51 +0530

    Time: 109 ms (35.55%), Space: 53.7 MB (68.26%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -18,31 +18,55 @@ class Solution {
     }
     
     
-//     private boolean subsetSumTabulationUtil(int N, int target, int[] nums) {
-//         boolean[][] dp = new boolean[N][target+1];
+    private boolean subsetSumTabulationUtil(int[] nums) {
+        int N = nums.length;
+        int totalSum = 0;
+        for(int i=0; i<N; i++) {
+            totalSum += nums[i];
+        }
         
-//         // Base - case
-//         for(int ind=0, ind<N; ind++) {
-//             dp[ind][0] = true;
-//         }
+        // Edge - case
+        if(N == 1 || totalSum%2 != 0) return false;
         
-//     }
+        int target = totalSum/2;
+        boolean[][] dp = new boolean[N][target+1];
+        
+        // Base - case
+        for(int ind=0; ind<N; ind++) {
+            dp[ind][0] = true;
+        }
+        
+        for(int ind=1; ind<N; ind++) {
+            for(int t=1; t<=target; t++) {
+                boolean notTake = dp[ind-1][t];
+                boolean take = false;
+                if(t >= nums[ind]) {
+                    take = dp[ind-1][t-nums[ind]];
+                }
+                
+                dp[ind][t] = (notTake || take);
+            }
+        }
+        return dp[N-1][target];
+    }
     
     public boolean canPartition(int[] nums) {
         
-        int totalSum = 0;
-        for(int e : nums) totalSum += e;
+//         int totalSum = 0;
+//         for(int e : nums) totalSum += e;
         
-        // Edge - case :: totalSum must be even
-        if(totalSum%2 != 0) return false;
+//         // Edge - case :: totalSum must be even
+//         if(totalSum%2 != 0) return false;
         
-        int N = nums.length-1;
-        int target = totalSum/2;
+//         int N = nums.length-1;
+//         int target = totalSum/2;
         
-        int[][] dp = new int[N][target+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
+//         int[][] dp = new int[N][target+1];
+//         for(int[] row : dp) Arrays.fill(row, -1);
         
-        return subsetSumUtil(N-1, target, nums, dp);
+//         return subsetSumUtil(N-1, target, nums, dp);
+        
+        return subsetSumTabulationUtil(nums);
         
     }
 }

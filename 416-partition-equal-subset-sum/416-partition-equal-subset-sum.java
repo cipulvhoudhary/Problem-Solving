383894e56c3ea9aea2be46050c683e72825f7ddd
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-10-05 11:29:17 +0530

    Time: 56 ms (71.71%), Space: 43 MB (81.45%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -19,6 +19,31 @@ class Solution {
         return (dp[ind][target] == 1) ? true : false;
     }
     
+    private boolean canPartitionTabulationUtil(int N, int[] nums, int target) {
+        boolean[][] dp = new boolean[N][target+1];
+        
+        // Base - case
+        for(int ind=0; ind<N; ind++) {
+            dp[ind][0] = true; // when target = 0
+        }
+        for(int t=1; t<=target; t++) {
+            if(nums[0] == t) dp[0][t] = true;
+        }
+        
+        // Main - logic
+        for(int ind=1; ind<N; ind++) {
+            for(int t=1; t<=target; t++) {
+                boolean notTake = dp[ind-1][t];
+                boolean take = false;
+                if(t >= nums[ind]) {
+                    take = dp[ind-1][t-nums[ind]];
+                }
+                dp[ind][t] = (notTake || take);
+            }
+        }
+        return dp[N-1][target];
+    }
+    
     public boolean canPartition(int[] nums) {
         int totalSum = 0;
         for(int e : nums) totalSum += e;
@@ -29,9 +54,11 @@ class Solution {
         int N = nums.length;
         int target = totalSum/2;
         
-        int[][] dp = new int[N][target+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
+//         int[][] dp = new int[N][target+1];
+//         for(int[] row : dp) Arrays.fill(row, -1);
         
-        return canPartitionMemoizationUtil(N-1, nums, target, dp);
+//         return canPartitionMemoizationUtil(N-1, nums, target, dp);
+        
+        return canPartitionTabulationUtil(N, nums, target);
     }
 }

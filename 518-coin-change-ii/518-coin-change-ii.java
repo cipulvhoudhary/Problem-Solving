393bd615b64a48ff0b6b8db80b799694acc10989
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-10-08 20:16:46 +0530

    Time: 18 ms (32.40%), Space: 50.9 MB (34.88%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.java
+++ 518-coin-change-ii/518-coin-change-ii.java
@@ -19,14 +19,41 @@ class Solution {
         return dp[ind][target];
     }
     
+    private int changeTabulationUtil(int N, int target, int[] coins) {
+        int[][] dp = new int[N][target+1];
+        
+        // Base - case
+        for(int ind=0; ind<N; ind++) dp[ind][0] = 1; // There is only one way
+        
+        for(int t=1; t<=target; t++) {
+            if(t%coins[0] == 0) dp[0][t] = 1;
+        }
+        
+        // Main - logic
+        for(int ind=1; ind<N; ind++) {
+            for(int t=1; t<=target; t++) {
+                int notTake = dp[ind-1][t];
+                int take = 0;
+                if(t >= coins[ind]) {
+                    take = dp[ind][t-coins[ind]];
+                }
+
+                dp[ind][t] = (notTake + take);
+            }
+        }
+        return dp[N-1][target];
+    }
+    
     public int change(int amount, int[] coins) {
         int N = coins.length;
         if(amount == 0) return 1;
         
-        int[][] dp = new int[N][amount+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
+//         int[][] dp = new int[N][amount+1];
+//         for(int[] row : dp) Arrays.fill(row, -1);
         
-        return changeUtil(N-1, amount, coins, dp);
+//         return changeUtil(N-1, amount, coins, dp);
+        
+        return changeTabulationUtil(N, amount, coins);
     }
     
 }

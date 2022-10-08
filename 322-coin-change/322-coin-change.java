bcddd1dd3a8507eaf2d4d78f6e8f03be75f69f66
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-10-08 12:12:16 +0530

    Time: 39 ms (38.29%), Space: 55 MB (17.67%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,6 +1,6 @@
 class Solution {
     
-    private int coinChangeUtil(int ind, int[] coins, int amount, int[][] dp) {
+    private int coinChangeMemoizationUtil(int ind, int[] coins, int amount, int[][] dp) {
         //  base - case
         if(ind == 0) {
             if(amount%coins[ind] == 0) return amount/coins[ind];
@@ -9,10 +9,10 @@ class Solution {
         
         // Main - logic
         if(dp[ind][amount] == -1) {
-            int notTake = coinChangeUtil(ind-1, coins, amount, dp);
+            int notTake = coinChangeMemoizationUtil(ind-1, coins, amount, dp);
             int take = 1000000000;
             if(amount >= coins[ind]) {
-                take = 1 + coinChangeUtil(ind, coins, amount-coins[ind], dp);
+                take = 1 + coinChangeMemoizationUtil(ind, coins, amount-coins[ind], dp);
             }
 
             dp[ind][amount] = Math.min(notTake, take);
@@ -20,13 +20,38 @@ class Solution {
         return dp[ind][amount];
     }
     
+    private int coinChangeTabulationUtil(int N, int[] coins, int amount) {
+        int[][] dp = new int[N][amount+1];
+        
+        for(int amt=0; amt<=amount; amt++) {
+            if(amt%coins[0] == 0) dp[0][amt] = amt/coins[0];
+            else dp[0][amt] = 1000000000;
+        }
+        
+        for(int ind=1; ind<N; ind++) {
+            for(int amt=0; amt<=amount; amt++) {
+                int notTake = dp[ind-1][amt];
+                int take = 1000000000;
+                if(amt >= coins[ind]) {
+                    take = 1 + dp[ind][amt-coins[ind]];
+                }
+
+                dp[ind][amt] = Math.min(notTake, take);
+            }
+        }
+        return dp[N-1][amount];
+    }
+    
     public int coinChange(int[] coins, int amount) {
         int N = coins.length;
         
-        int[][] dp = new int[N][amount+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
+//         int[][] dp = new int[N][amount+1];
+//         for(int[] row : dp) Arrays.fill(row, -1);
         
-        int ans = coinChangeUtil(N-1, coins, amount, dp);
+//         int ans = coinChangeMemoizationUtil(N-1, coins, amount, dp);
+        
+        int ans = coinChangeTabulationUtil(N, coins, amount);
+        
         if(ans == 1000000000) return -1;
         return ans;
     }

b264a381b0924be25536950f63dccf702f3f0d2a
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-11 18:18:30 +0530

    Time: 2 ms (99.25%), Space: 42.4 MB (90.30%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,17 +10,31 @@
  */
 class Solution {
     
+    // TC --> O(M+N) || SC --> O(M+N)
+    // M : length of list1 
+    // N :: length of list2
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        // Edge - case
+        // Edge - case :: if any list is null, return the other list
         if(l1 == null) return l2;
         if(l2 == null) return l1;
         
         ListNode t1 = l1, t2 = l2;
-        ListNode res = new ListNode(0); // dummy node
+        
+        // dummy node
+        // Taking dummy node to avoid multiple if-else
+        ListNode res = new ListNode(0); 
         ListNode t3 = res;
         
         int carry = 0;
+        
+        // Approach -->
+        // till any one of the list has some nodes, it will be considered in the sum
+        // Think of it like a basic addition we do perform of decimal number system
+        // sum = a+b
+        // if a+b >= 10 , we take mod of (a+b) with 10 (because dealing with decimal number system)
+        // and place mod value below a and b
+        // and take carry as sum / 10
         
         while(t1 != null || t2 != null) {
             int sum = carry;
@@ -44,6 +58,13 @@ class Solution {
             
             carry = sum / 10;
         }
+        
+        //If carry is left
+        //            1 1 1 1(carry which got left) 
+        // l1 --> 9 9 9 9 
+        // l2 --> 9 9
+        //        8 9 0 0 1
+            
         if(carry != 0) {
             ListNode node = new ListNode(carry);
             t3.next = node;

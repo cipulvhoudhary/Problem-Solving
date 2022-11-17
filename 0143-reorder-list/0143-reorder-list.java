671b58e736557e3a87cd814b2d139306b19ec3a3
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-17 13:51:42 +0530

    Time: 11 ms (25.40%), Space: 54 MB (57.17%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,8 +10,19 @@
  */
 class Solution {
     
+    private ListNode getFirstMid(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    
     private ListNode reverse(ListNode head) {
         ListNode prev = null, curr = head;
+        
         while(curr != null) {
             ListNode temp = curr.next;
             curr.next = prev;
@@ -21,54 +32,101 @@ class Solution {
         return prev;
     }
     
-    private ListNode clone(ListNode head, int[] len) {
-        ListNode t1 = head;
-        ListNode newHead = new ListNode(0);
-        ListNode t2 = newHead;
-        
-        while(t1 != null) {
-            len[0]++;
-            ListNode node = new ListNode(t1.val);
-            t2.next = node;
-            t2 = t2.next;
-            t1 = t1.next;
-        }
-        newHead = newHead.next;
-        return newHead;
-    }
-    
     public void reorderList(ListNode head) {
-        int[] len = new int[1];
-        
-        ListNode head2 = clone(head, len);
-        // System.out.print(len[0]);
-        head2 = reverse(head2);
+        ListNode mid = getFirstMid(head);
+        mid.next = reverse(mid.next);
+        ListNode head2 = mid.next;
+        mid.next = null;
         
         ListNode t1 = head;
         ListNode t2 = head2;
         
         // Dummy node
         ListNode res = new ListNode(0);
-        ListNode temp = res;
+        ListNode t3 = res;
         
-        int count = 0;
-        
-        while(count < len[0]) {
-            
-            if(count % 2 == 0) {
-                temp.next = t1;
+        int num = 0;
+        while(t1 != null || t2 != null) {
+            if(num % 2 == 0) {
+                t3.next = t1;
                 t1 = t1.next;
             }
-            else  {
-                temp.next = t2;
+            else {
+                t3.next = t2;
                 t2 = t2.next;
             }
-            temp = temp.next;
-            count++;
+            num++;
+            t3 = t3.next;
         }
-        temp.next = null;
-        
         res = res.next;
         head = res;
     }
 }
+
+
+
+// Working code :: Not optimal, takes O(N) space
+
+// class Solution {
+//     private ListNode reverse(ListNode head) {
+//         ListNode prev = null, curr = head;
+//         while(curr != null) {
+//             ListNode temp = curr.next;
+//             curr.next = prev;
+//             prev = curr;
+//             curr = temp;
+//         }
+//         return prev;
+//     }
+    
+//     private ListNode clone(ListNode head, int[] len) {
+//         ListNode t1 = head;
+//         ListNode newHead = new ListNode(0);
+//         ListNode t2 = newHead;
+        
+//         while(t1 != null) {
+//             len[0]++;
+//             ListNode node = new ListNode(t1.val);
+//             t2.next = node;
+//             t2 = t2.next;
+//             t1 = t1.next;
+//         }
+//         newHead = newHead.next;
+//         return newHead;
+//     }
+    
+//     public void reorderList(ListNode head) {
+//         int[] len = new int[1];
+        
+//         ListNode head2 = clone(head, len);
+//         // System.out.print(len[0]);
+//         head2 = reverse(head2);
+        
+//         ListNode t1 = head;
+//         ListNode t2 = head2;
+        
+//         // Dummy node
+//         ListNode res = new ListNode(0);
+//         ListNode temp = res;
+        
+//         int count = 0;
+        
+//         while(count < len[0]) {
+            
+//             if(count % 2 == 0) {
+//                 temp.next = t1;
+//                 t1 = t1.next;
+//             }
+//             else  {
+//                 temp.next = t2;
+//                 t2 = t2.next;
+//             }
+//             temp = temp.next;
+//             count++;
+//         }
+//         temp.next = null;
+        
+//         res = res.next;
+//         head = res;
+//     }
+// }

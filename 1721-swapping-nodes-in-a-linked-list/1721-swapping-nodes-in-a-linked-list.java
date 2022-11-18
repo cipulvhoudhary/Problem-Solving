dab6d0733af5266a7d8e8aabeda2246966f88083
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-18 13:54:14 +0530

    Time: 4 ms (66.23%), Space: 172.3 MB (54.53%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -35,26 +35,41 @@ class Solution {
     }
     
     // Main function
+    // Two pass
     // TC --> O(N) || SC --> O(1)
     public ListNode swapNodes(ListNode head, int k) {
         if(head.next == null) return head;
         
-        int len = getLength(head); // Helper function 1
-        
-        ListNode t1 = head, t2 = head;
-        int c1 = k-1, c2 = (len-k);
-        while(c1 > 0 || c2 > 0) {
-            if(c1 > 0) {
-                t1 = t1.next;
-                c1--;
+        // Approach 1 : Single pass
+        ListNode t1 = null, t2 = null, temp = head;
+        while(temp != null) {
+            --k;
+            if(t2 != null) t2 = t2.next;
+            if(k == 0) {
+                t1 = temp; // first node found at kth index from start
+                t2 = head;
             }
-            if(c2 > 0) {
-                t2 = t2.next;
-                c2--;
-            }
+            temp = temp.next;
         }
         
         swap(t1, t2); // Helper function 2
         return head;
+        
+        // Approach 2 :: double pass
+        // int len = getLength(head); // Helper function 1
+        // ListNode t1 = head, t2 = head;
+        // int c1 = k-1, c2 = (len-k);
+        // while(c1 > 0 || c2 > 0) {
+        //     if(c1 > 0) {
+        //         t1 = t1.next;
+        //         c1--;
+        //     }
+        //     if(c2 > 0) {
+        //         t2 = t2.next;
+        //         c2--;
+        //     }
+        // }
+        // swap(t1, t2); // Helper function 2
+        // return head;
     }
 }

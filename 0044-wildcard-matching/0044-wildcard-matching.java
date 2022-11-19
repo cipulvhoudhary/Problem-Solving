46ed35085d1cf15871a011da6ce508adec2f2e07
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-20 01:27:12 +0530

    Time: 44 ms (56.12%), Space: 43.7 MB (87.83%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,12 +1,12 @@
 class Solution {
     
-    private boolean isMatchUtil(int lenS, int lenP, String s, String p, int[][] dp) {
+    private boolean isMatchMemoization(int lenS, int lenP, String s, String p, int[][] dp) {
         // Base - case
         if(lenS ==  0 && lenP == 0) return true;
         if(lenP == 0) return false;
         if(lenS == 0) {
             if(p.charAt(lenP-1) == '*') {
-                return isMatchUtil(lenS, lenP-1, s, p, dp); 
+                return isMatchMemoization(lenS, lenP-1, s, p, dp); 
             }
             return false;
         }
@@ -14,10 +14,10 @@ class Solution {
         // Main - logic
         if(dp[lenS][lenP] == -1) {
             if(p.charAt(lenP-1) == '?' || s.charAt(lenS-1) == p.charAt(lenP-1)) {
-                dp[lenS][lenP] = (isMatchUtil(lenS-1, lenP-1, s, p, dp) ? 1 : 0);
+                dp[lenS][lenP] = (isMatchMemoization(lenS-1, lenP-1, s, p, dp) ? 1 : 0);
             }
             else if(p.charAt(lenP-1) == '*') {
-                dp[lenS][lenP] = ((isMatchUtil(lenS, lenP-1, s, p, dp) || isMatchUtil(lenS-1, lenP, s, p, dp)) ? 1 : 0);
+                dp[lenS][lenP] = ((isMatchMemoization(lenS, lenP-1, s, p, dp) || isMatchMemoization(lenS-1, lenP, s, p, dp)) ? 1 : 0);
             }
             else {
                 dp[lenS][lenP] = 0;
@@ -26,12 +26,43 @@ class Solution {
         return ((dp[lenS][lenP]==1) ? true : false); 
     }
     
+    private boolean isMatchTabulation(String s, String p) {
+        int lengthS = s.length(), lengthP = p.length();
+        int[][] dp = new int[lengthS+1][lengthP+1];
+        
+        // Base - case
+        dp[0][0] = 1;
+        for(int lenP=1; lenP<=lengthP; lenP++) {
+            if(p.charAt(lenP-1) == '*') {
+                dp[0][lenP] = dp[0][lenP-1];
+            }
+        }
+        
+        // Main - logic
+        for(int lenS=1; lenS<=lengthS; lenS++) {
+            for(int lenP=1; lenP<=lengthP; lenP++) {
+                if(p.charAt(lenP-1) == '?' || s.charAt(lenS-1) == p.charAt(lenP-1)) {
+                    dp[lenS][lenP] = dp[lenS-1][lenP-1];
+                }
+                else if(p.charAt(lenP-1) == '*') {
+                    dp[lenS][lenP] = ((dp[lenS][lenP-1]) | (dp[lenS-1][lenP]));
+                }
+                else {
+                    dp[lenS][lenP] = 0;
+                }
+            }
+        }
+        return ((dp[lengthS][lengthP]==1) ? true : false);
+    }
+    
     public boolean isMatch(String s, String p) {
-        int lenS = s.length(), lenP = p.length();
+//         int lenS = s.length(), lenP = p.length();
         
-        int[][] dp = new int[lenS+1][lenP+1];
-        for(int[] row : dp)  Arrays.fill(row, -1);
+//         int[][] dp = new int[lenS+1][lenP+1];
+//         for(int[] row : dp)  Arrays.fill(row, -1);
         
-        return isMatchUtil(lenS, lenP, s, p, dp);
+//         return isMatchMemoization(lenS, lenP, s, p, dp);
+        
+        return isMatchTabulation(s, p);
     }
 }

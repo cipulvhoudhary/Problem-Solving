ef6d8d6191877b987a5cf35534140f8a14bf6719
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-24 18:38:40 +0530

    Time: 0 ms (100.00%), Space: 40 MB (82.53%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    
+    // Memoization 
+    // TC --> O(N) || SC --> O(N)
+    private int houseRobber1Util(int s, int e, int[] nums, int[] dp) {
+        // Base - case
+        if(e == s) return nums[e];
+        if(e == s+1) return (Math.max(nums[s], nums[e]));
+        
+        // Main - logic
+        if(dp[e] == -1) {
+            int notRobbed = houseRobber1Util(s, e-1, nums, dp);
+            int robbed = nums[e] + houseRobber1Util(s, e-2, nums, dp); 
+
+            dp[e] = Math.max(notRobbed, robbed);
+        }
+        return dp[e];
+    }
+    
+    // Tabulation
+    
+    private int houseRobber1Tabulation(int s, int e, int[] nums) {
+        int[] dp = new int[e - s + 1];
+        
+        dp[0] = nums[s]; // if only single house is present
+        if(s < e) dp[1] = Math.max(nums[s], nums[s+1]); // if 2 or more than one house is there
+        
+        for(int ind=s+2; ind<=e; ind++) {
+            int notRobbed = 0 + dp[ind-1-s];
+            int robbed = nums[ind] + dp[ind-2-s];
+            
+            dp[ind-s] = Math.max(notRobbed, robbed);
+        }
+        return dp[e-s];
+    }
+    
+    private int houseRobber1(int s, int e, int[] nums) {
+        
+        // Memoization 
+        // int N = nums.length;
+        // int[] dp = new int[N];
+        // Arrays.fill(dp, -1);
+        // return houseRobber1Util(s, e, nums, dp);
+        
+        // Tabulation
+        return houseRobber1Tabulation(s, e, nums);
+    }
+    
+    
+    public int rob(int[] nums) {
+        /* 
+        Approach 
+        if we break the circle 
+        robber can now rob just like house robber problem
+        1st time --> can rob [0, N-2] 
+        2nd time --> can rob [1, N-1]
+        the maximum money robbed in both the solution is the answer
+        */
+        int N = nums.length;
+        
+        // Edge - case
+        if(N == 1) return nums[0];
+        
+        int rob1 = houseRobber1(0, N-2, nums);
+        int rob2 = houseRobber1(1, N-1, nums);
+        return Math.max(rob1, rob2);
+    }
+    
+}

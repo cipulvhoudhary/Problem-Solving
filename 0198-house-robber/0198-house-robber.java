b7f6b6e036124ccdf03de4eb688cd385ee79a2b6
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-24 10:44:36 +0530

    Time: 1 ms (22.74%), Space: 41.5 MB (43.35%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,5 +1,7 @@
 class Solution {
     
+    // Approach 1 :: Memoization
+    // TC --> O(N) || SC --> O(N)
     private int robMemoization(int ind, int[] nums, int[] dp) {
         // Base - case
         if(ind == 0) return nums[ind];
@@ -15,6 +17,53 @@ class Solution {
         return dp[ind];
     }
     
+    // Approach 2 :: Tabulation
+    // TC --> O(N) || SC --> O(N)
+    private int robTabulation(int[] nums) {
+        int N = nums.length;
+        int[] dp = new int[N];
+        
+        // Base - case
+        dp[0] = nums[0];
+        if(N > 1) {
+            dp[1] = Math.max(nums[0], nums[1]);
+        }
+        
+        for(int ind=2; ind<N; ind++) {
+            int notRobbed = 0 + dp[ind-1];
+            int robbed = nums[ind] + dp[ind-2];
+
+            dp[ind] = Math.max(notRobbed, robbed);
+        }
+        return dp[N];
+    }
+    
+    // Approach 3 :: Space - optimized
+    // TC --> O(N) || SC --> O(1)
+    private int robSpaceOptimized(int[] nums) {
+        int N = nums.length;
+        
+        // Base - case
+        if(N == 1) return nums[0];
+        
+        int prev2 = nums[0];
+        int prev = 0;
+        if(N > 1) {
+            prev = Math.max(nums[0], nums[1]);
+        }
+        
+        for(int ind=2; ind<N; ind++) {
+            int notRobbed = 0 + prev;
+            int robbed = nums[ind] + prev2;
+
+            int curr = Math.max(notRobbed, robbed);
+            
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+    
     public int rob(int[] nums) {
         /*
         Approach 
@@ -23,9 +72,17 @@ class Solution {
         if the robber does not rob ith house, he get 0 coins + he can rob the (i-1)th house...
         ... without alerting the police
         */
-        int N = nums.length;
-        int[] dp = new int[N];
-        Arrays.fill(dp, -1);
-        return robMemoization(N-1, nums, dp);
+        
+        // Approach 1 :: Memoization
+        // int N = nums.length;
+        // int[] dp = new int[N];
+        // Arrays.fill(dp, -1);
+        // return robMemoization(N-1, nums, dp);
+        
+        // Approach 2 :: Tabulation
+        // return robTabulation(nums);
+        
+        // Approach 3 :: Space - optimized
+        return robSpaceOptimized(nums);
     }
 }

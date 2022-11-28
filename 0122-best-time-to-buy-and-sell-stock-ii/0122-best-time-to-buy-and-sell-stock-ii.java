46740c483a88da23fc6f59fa8b6951860c0bbcdb
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-28 19:13:43 +0530

    Time: 11 ms (6.03%), Space: 48.3 MB (5.06%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,20 +1,27 @@
 class Solution {
+    
+    // Approach 1 :: Memoization
+    // TC --> O(N) || SC --> O(N)
     // canBuy --> true (can buy) || canBuy --> false (already bought)
-    private int maxProfitUtil(int ind, int canBuy, int[] prices, int[][] dp) {
+    private int maxProfitMemoization(int ind, int canBuy, int[] prices, int[][] dp) {
         // Base - case
-        if(ind == prices.length) return 0;
+        // if(ind == prices.length) return 0;
+        if(ind == prices.length-1) {
+            if(canBuy == 1) return 0;
+            else return prices[prices.length-1];
+        }
         
         // Main - logic
         if(dp[ind][canBuy] == -1) {
             int profit = 0;
             if(canBuy == 1) {
-                int buy = -prices[ind] + maxProfitUtil(ind+1, 0, prices, dp);
-                int notBuy = 0 +  maxProfitUtil(ind+1, 1, prices, dp);
+                int buy = -prices[ind] + maxProfitMemoization(ind+1, 0, prices, dp);
+                int notBuy = 0 +  maxProfitMemoization(ind+1, 1, prices, dp);
                 profit = Math.max(buy, notBuy);
             }
             else {
-                int sell = prices[ind] +  maxProfitUtil(ind+1, 1, prices, dp);
-                int notSell = 0 +  maxProfitUtil(ind+1, 0, prices, dp);
+                int sell = prices[ind] + maxProfitMemoization(ind+1, 1, prices, dp);
+                int notSell = 0 + maxProfitMemoization(ind+1, 0, prices, dp);
                 profit = Math.max(sell, notSell);
             }
             dp[ind][canBuy] = profit;
@@ -22,12 +29,43 @@ class Solution {
         return dp[ind][canBuy];
     }
     
+    // Approach 2 :: Tabulation
+    private int maxProfitTabulation(int[] prices) {
+        int N = prices.length;
+        int[][] dp = new int[N][2]; 
+        
+        dp[N-1][0] = prices[N-1];
+        // base case is already covered with the initial config of dp[as array of int initializes from 0]
+        
+        for(int ind=N-2; ind>=0; ind--) {
+            int profit = 0;
+            for(int canBuy=1; canBuy>=0; canBuy--) {
+                if(canBuy == 1) {
+                    int buy = -prices[ind] + dp[ind+1][0];
+                    int notBuy = 0 + dp[ind+1][1];
+                    profit = Math.max(buy, notBuy);
+                }
+                else {
+                    int sell = prices[ind] + dp[ind+1][1];
+                    int notSell = 0 + dp[ind+1][0];
+                    profit = Math.max(sell, notSell);
+                }
+                dp[ind][canBuy] = profit;
+            }
+        }
+        return dp[0][1];
+    }
+    
     public int maxProfit(int[] prices) {
         int N = prices.length;
         
+        // Approach 1 :: Memoization
         int[][] dp = new int[N][2];
         for(int[] row : dp) Arrays.fill(row, -1);
         
-        return maxProfitUtil(0, 1, prices, dp);
+        return maxProfitMemoization(0, 1, prices, dp);
+        
+        // Approach 2 :: Tabulation
+        // return maxProfitTabulation(prices);
     }
 }

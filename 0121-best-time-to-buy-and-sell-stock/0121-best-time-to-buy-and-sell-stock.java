b0915f4c6548e657d9a3175e896129ab28e65d3f
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-28 17:17:34 +0530

    Time: 7 ms (20.76%), Space: 84.2 MB (21.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -9,16 +9,30 @@ class Solution {
         we can square off to get the profit/loss
         Right max array will help
         */
-        int[] rMax = new int[N];
-        rMax[N-1] = prices[N-1];
-        for(int i=N-2; i>=0; i--) {
-            rMax[i] = Math.max(prices[i], rMax[i+1]);
-        }
+//         int[] rMax = new int[N];
+//         rMax[N-1] = prices[N-1];
+//         for(int i=N-2; i>=0; i--) {
+//             rMax[i] = Math.max(prices[i], rMax[i+1]);
+//         }
                 
+//         int maxProfit = 0;
+//         for(int i=0; i<N-1; i++) {
+//             int profit = rMax[i+1] - prices[i];
+//             maxProfit = Math.max(maxProfit, profit);
+//         }
+//         return maxProfit;
+        
+        /* Approach 2 
+        TC --> O(N) || SC -- > O(1)
+        Traverse from last, and carry rMax as a int literal
+        */
+        int rMax = prices[N-1];
         int maxProfit = 0;
-        for(int i=0; i<N-1; i++) {
-            int profit = rMax[i+1] - prices[i];
+        for(int i=N-2; i>=0; i--) {
+            int profit = rMax - prices[i];
             maxProfit = Math.max(maxProfit, profit);
+            
+            rMax = Math.max(rMax, prices[i]);
         }
         return maxProfit;
     }

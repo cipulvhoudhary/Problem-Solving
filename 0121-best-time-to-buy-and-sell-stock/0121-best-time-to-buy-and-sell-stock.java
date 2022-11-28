48c8609124d4662041d76c8b1701e4de92cd1279
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-11-28 17:11:53 +0530

    Time: 5 ms (40.25%), Space: 85.7 MB (5.13%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int N = prices.length;
+        
+        /* Approach 1
+        TC --> O(N) || O(N)
+        for every ith day, we buy at price[i], 
+        if we somehow know the max price[i] from future i.e price --> [i+1] till [N-1]
+        we can square off to get the profit/loss
+        Right max array will help
+        */
+        int[] rMax = new int[N];
+        rMax[N-1] = prices[N-1];
+        for(int i=N-2; i>=0; i--) {
+            rMax[i] = Math.max(prices[i], rMax[i+1]);
+        }
+                
+        int maxProfit = 0;
+        for(int i=0; i<N-1; i++) {
+            int profit = rMax[i+1] - prices[i];
+            maxProfit = Math.max(maxProfit, profit);
+        }
+        return maxProfit;
+    }
+}

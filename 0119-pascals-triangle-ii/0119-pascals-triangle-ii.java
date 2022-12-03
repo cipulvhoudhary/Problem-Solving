508452a07da31ec52f58ca6da1675cb3e03a8dde
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-12-03 17:36:44 +0530

    Time: 1 ms (91.77%), Space: 40.1 MB (84.55%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -21,8 +21,30 @@ class Solution {
         return currList;
     }
     
-    public List<Integer> getRow(int rowIndex) {        
-        List<Integer> res = getRowUtil(rowIndex);
-        return res;
+    private List<Integer> getRowSpaceOptimized(int rowIndex) {
+        List<Integer> prevList = new ArrayList<>();
+        prevList.add(1);
+            
+        for(int ri=1; ri<=rowIndex; ri++) {
+            List<Integer> currList = new ArrayList<>();
+            currList.add(1);
+            for(int i=0; i<prevList.size()-1; i++) {
+                currList.add(prevList.get(i) + prevList.get(i+1));
+            }
+            currList.add(1);
+            
+            // change reference of currList to prevList
+            prevList = currList;
+        }
+        return prevList;
+    }
+    
+    public List<Integer> getRow(int rowIndex) {    
+        // Approach - 1 
+        // return getRowUtil(rowIndex);        
+        
+        // Approach - 2
+        return getRowSpaceOptimized(rowIndex);
+        
     }
 }

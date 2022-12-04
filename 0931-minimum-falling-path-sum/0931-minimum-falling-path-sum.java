d84002dc498952f135cf675283d0affcf9975372
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-12-04 22:35:29 +0530

    Time: 5 ms (64.06%), Space: 42.8 MB (92.14%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -19,18 +19,55 @@ class Solution {
         return dp[i][j];
     }
     
+    private int minFallingPathSumTabulation(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n][n];
+        
+        // Base - case
+        for(int j=0; j<n; j++) {
+            dp[n-1][j] = matrix[n-1][j];
+        }
+        
+        // Main - logic
+        for(int i=n-2; i>=0; i--) {
+            for(int j=0; j<n; j++) {
+                int bottom = dp[i+1][j];
+                int bottomLeft = Integer.MAX_VALUE;
+                int bottomRight = Integer.MAX_VALUE;
+                if(j-1 >= 0 && j-1 < n) {
+                    bottomLeft = dp[i+1][j-1];
+                }
+                if(j+1 >= 0 && j+1 < n) {
+                    bottomRight = dp[i+1][j+1];
+                }
+                
+                dp[i][j] = matrix[i][j] + Math.min(bottom, Math.min(bottomLeft, bottomRight));
+            }
+        }
+        
+        // Compute min falling sum. 
+        int minFallingSum = Integer.MAX_VALUE;
+        for(int j=0; j<n; j++) {
+            minFallingSum = Math.min(minFallingSum, dp[0][j]);
+        }
+        
+        return minFallingSum;
+    }
+    
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
         
-        int minFallingSum = Integer.MAX_VALUE;
-        for(int j=0; j<n; j++) {
-            
-            int[][] dp = new int[n][n];
-            for(int[] row : dp) Arrays.fill(row, -1);
-            
-            int fallingSum = minFallingPathSumUtil(0, j,  matrix, n, dp);
-            minFallingSum = Math.min(minFallingSum, fallingSum);
-        }
-        return minFallingSum;
+        // Approach 1 :: Memoization
+        // int minFallingSum = Integer.MAX_VALUE;
+        // for(int j=0; j<n; j++) {
+        //     int[][] dp = new int[n][n];
+        //     for(int[] row : dp) Arrays.fill(row, -1);
+        //     int fallingSum = minFallingPathSumUtil(0, j,  matrix, n, dp);
+        //     minFallingSum = Math.min(minFallingSum, fallingSum);
+        // }
+        // return minFallingSum;
+        
+        // Approach 2 :: Tabulation
+        return minFallingPathSumTabulation(matrix);
     }
 }

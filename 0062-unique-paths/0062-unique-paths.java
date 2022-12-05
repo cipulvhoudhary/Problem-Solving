4d141fa381478b1cdf871862cad4b64fb1694ba5
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-12-05 23:15:14 +0530

    Time: 2 ms (31.61%), Space: 40.7 MB (59.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -23,14 +23,31 @@ class Solution {
         
         for(int i=1; i<m; i++) {
             for(int j=1; j<n; j++) {
-                int top = uniquePathsUtil(i-1, j, m, n, dp);
-                int left = uniquePathsUtil(i, j-1, m, n, dp); 
+                int top = dp[i-1][j];
+                int left = dp[i][j-1]; 
 
                 dp[i][j] = top + left;
             }
         }
         return dp[m-1][n-1];
     }
+    
+    
+//     private int uniquePathsSpaceOptimized(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         dp[0][0] = 1;
+//         for(int j=1; j<n; j++) dp[0][j] = 1;
+        
+//         for(int i=1; i<m; i++) {
+//             for(int j=1; j<n; j++) {
+//                 int top = dp[i-1][j];
+//                 int left = dp[i][j-1]; 
+
+//                 dp[i][j] = top + left;
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
     
     public int uniquePaths(int m, int n) {
         
@@ -39,5 +56,7 @@ class Solution {
         // return uniquePathsUtil(m-1, n-1, m, n, dp);
         
         return uniquePathsTabulation(m, n);
+        
+        // return uniquePathsSpaceOptimized(m, n);
     }
 }

7032d3866524420e8c725a3d6cc87281d44106c0
Vipul Kumar Choudhary <109037335+cipulvhoudhary@users.noreply.github.com>
2022-12-05 23:12:22 +0530

    Time: 1 ms (71.33%), Space: 41.1 MB (36.31%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -15,11 +15,29 @@ class Solution {
         return dp[i][j];
     }
     
+    private int uniquePathsTabulation(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for(int i=1; i<m; i++) dp[i][0] = 1;
+        for(int j=1; j<n; j++) dp[0][j] = 1;
+        
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                int top = uniquePathsUtil(i-1, j, m, n, dp);
+                int left = uniquePathsUtil(i, j-1, m, n, dp); 
+
+                dp[i][j] = top + left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
     public int uniquePaths(int m, int n) {
         
-        int[][] dp = new int[m][n];
-        for(int[] row : dp) Arrays.fill(row, -1);
+        // int[][] dp = new int[m][n];
+        // for(int[] row : dp) Arrays.fill(row, -1);
+        // return uniquePathsUtil(m-1, n-1, m, n, dp);
         
-        return uniquePathsUtil(m-1, n-1, m, n, dp);
+        return uniquePathsTabulation(m, n);
     }
 }
